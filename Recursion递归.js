ea61f578d62377c4697b41fbc838a50739f6b67e
xmy <48195327+xmy-dot@users.noreply.github.com>
2023-01-13 17:03:10 +0800

    兔子

--- Recursion递归.js
+++ Recursion递归.js
@@ -12,8 +12,8 @@ function factorial(ranNum) {
   }
 }
 const result1 = factorial(ranNum);
-console.log("ranNum", ranNum);
-console.log("result1", result1);
+// console.log("ranNum", ranNum);
+// console.log("result1", result1);
 
 const data = [
   {
@@ -57,5 +57,28 @@ function findObj(data, id) {
   return result;
 }
 
-const res = findObj(data, 11);
-console.log(res);
+const res = findObj(data, 211);
+// console.log(res);
+
+//假定一对大兔子每月能生一对小兔子 每对小兔子经过一个月能长成一对大兔子具备繁殖能力
+//不发生死亡 每次均生下一公一母
+//问最初有一对小兔子 n个月后有多少对兔子
+// let rabbitNum = 1;
+function rabbit(n) {
+  let rabbitNum = 1;
+  // 月份 0 1 2 3 4 5
+  //  大  0 1 1 2 3 5
+  //  小  1 0 1 1 2 3
+  //  总  1 1 2 3 5 8
+  // 总结可知 第三个月的总兔子数等于前两个月的兔子数之和
+  // 即rabbit(m) = rabbit(m-1) + rabbit(m-2)
+  if (n === 0) {
+    rabbitNum = 1;
+  } else if (n === 1) {
+    rabbitNum = 1;
+  } else {
+    rabbitNum = rabbit(n - 2) + rabbit(n - 1);
+  }
+  return rabbitNum;
+}
+// console.log(rabbit(9));
