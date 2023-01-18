41f8de8116078003a7712a7f421b26a90a78523f
xmy <48195327+xmy-dot@users.noreply.github.com>
2023-01-19 01:31:55 +0800

    兔子

--- 兔子.js
+++ 兔子.js
@@ -4,21 +4,20 @@
 //不发生死亡 每次均生下一公一母
 //问最初有一对小兔子 n个月后有多少对兔子
 // let rabbitNum = 1;
+// 月份 0 1 2 3 4 5
+//  大  0 1 1 2 3 5
+//  小  1 0 1 1 2 3
+//  总  1 1 2 3 5 8
+// 总结可知 第三个月的总兔子数等于前两个月的兔子数之和
+// 即rabbit(m) = rabbit(m-1) + rabbit(m-2)
 function rabbit(n) {
-    let rabbitNum = 1;
-    // 月份 0 1 2 3 4 5
-    //  大  0 1 1 2 3 5
-    //  小  1 0 1 1 2 3
-    //  总  1 1 2 3 5 8
-    // 总结可知 第三个月的总兔子数等于前两个月的兔子数之和
-    // 即rabbit(m) = rabbit(m-1) + rabbit(m-2)
-    if (n === 0) {
-      rabbitNum = 1;
-    } else if (n === 1) {
-      rabbitNum = 1;
-    } else {
-      rabbitNum = rabbit(n - 2) + rabbit(n - 1);
-    }
-    return rabbitNum;
+  let rabbitNum = 1;
+  if (n === 0) {
+    rabbitNum = 1;
+  } else if (n === 1) {
+    rabbitNum = 1;
+  } else {
+    rabbitNum = rabbit(n - 2) + rabbit(n - 1);
   }
-  // console.log(rabbit(9));
+  return rabbitNum;
+}
