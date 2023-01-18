71ba5181475149e04cdeb757f71493c00e76dcb8
xmy <48195327+xmy-dot@users.noreply.github.com>
2023-01-19 01:34:25 +0800

    阶乘

--- 阶乘factorial.js
+++ 阶乘factorial.js
@@ -2,17 +2,14 @@
 
 // 取0到20中的一个随机整数
 function randomNum() {
-    return Math.floor(Math.random() * 20);
+  return Math.floor(Math.random() * 20);
+}
+const ranNum = randomNum();
+//递归求随机数ranNum的阶乘
+function factorial(ranNum) {
+  if (ranNum === 0 || ranNum === 1) {
+    return 1;
+  } else {
+    return factorial(ranNum - 1) * ranNum;
   }
-  const ranNum = randomNum();
-  //递归求随机数ranNum的阶乘
-  function factorial(ranNum) {
-    if (ranNum === 0 || ranNum === 1) {
-      return 1;
-    } else {
-      return factorial(ranNum - 1) * ranNum;
-    }
-  }
-  // const result = factorial(ranNum);
-  // console.log("ranNum", ranNum);
-  // console.log("result", result);
+}
