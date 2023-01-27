a8da410b7e87d6d79deae0c4e590ec5896f854c1
xmy <xmy@123.com>
2023-01-27 17:01:02 +0800

    兼具大小写的最好的英文字母

---
+++ 兼具大小写的最好英文字母.js
@@ -0,0 +1,44 @@
+// leetCode 2309
+
+// 给你一个由英文字母组成的字符串 s
+//请你找出并返回 s 中的最好英文字母
+//返回的字母必须为大写形式
+//如果不存在满足条件的字母，则返回一个空字符串
+// 最好英文字母的大写和小写形式必须都在 s 中出现。
+// 英文字母 b 比另一个英文字母 a 更好 的前提是：英文字母表中，b 在 a 之后出现。
+
+// 示例 1：
+// 输入：s = "lEeTcOdE"
+// 输出："E"
+// 解释：
+// 字母 'E' 是唯一一个大写和小写形式都出现的字母。
+// 示例 2：
+// 输入：s = "arRAzFif"
+// 输出："R"
+// 解释：
+// 字母 'R' 是大写和小写形式都出现的最好英文字母。
+// 注意 'A' 和 'F' 的大写和小写形式也都出现了，但是 'R' 比 'F' 和 'A' 更好。
+// 示例 3：
+// 输入：s = "AbCdEfGhIjK"
+// 输出：""
+// 解释：
+// 不存在大写和小写形式都出现的字母。
+
+// 提示：
+// 1 <= s.length <= 1000
+// s 由小写和大写英文字母组成
+
+var greatestLetter = function (s) {
+  let str = s.split("");
+  str.forEach((element, index, array) => {
+    array[index] = element.charCodeAt();
+  });
+  let res = "";
+  for (let index = 90; index >= 65; index--) {
+    if (str.indexOf(index) >= 0 && str.indexOf(index + 32) >= 0) {
+      res = String.fromCharCode(index);
+      break;
+    }
+  }
+  return res;
+};
