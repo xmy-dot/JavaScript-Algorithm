7f75efbe39c5b1511dc65e93e6f74a15092c1cb3
xmy <48195327+xmy-dot@users.noreply.github.com>
2023-01-31 02:00:00 +0800

    Update 有效的山脉数组.js

--- 有效的山脉数组.js
+++ 有效的山脉数组.js
@@ -1,13 +1,12 @@
 //leetCode 941
 
 // 给定一个整数数组 arr，如果它是有效的山脉数组就返回 true，否则返回 false。
-
 // 如果 arr 满足下述条件，那么它是一个山脉数组：
 
 // 1. arr.length >= 3
 // 2. 在 0 < i < arr.length - 1 条件下，存在 i 使得：
-//    arr[0] < arr[1] < ... arr[i-1] < arr[i]
-//    arr[i] > arr[i+1] > ... > arr[arr.length - 1]
+   // arr[0] < arr[1] < ... arr[i-1] < arr[i]
+   // arr[i] > arr[i+1] > ... > arr[arr.length - 1]
 
 // 示例 1：
 // 输入：arr = [2,1]
